7ff8b90dda6bf58402971f08442ddf6efd200b71
DmitriiFedorov <fdorov_dima@mail.ru>
2021-11-01 20:04:19 +0500

    5

--- lesson_1/geekshop/mainapp/views.py
+++ lesson_1/geekshop/mainapp/views.py
@@ -1,6 +1,10 @@
+from django.http import request
 from django.shortcuts import render
+
+from basketapp.models import Basket
 from mainapp.models import Product
 from .models import ProductCategory
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -19,10 +23,39 @@ def contact(request):
 
 
 def products(request, pk=None):
+    print(pk)
+
+    title = 'продукты'
+    links_menu = ProductCategory.objects.all()
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
+    if pk:
+        if pk == '0':
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        context = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+            'basket': basket,
+        }
+        return render(request, 'mainapp/products_list.html', context)
+
+    same_products = Product.objects.all()[3:5]
 
     context = {
-        'links_menu': ProductCategory.objects.all(),
-        'title': 'продукты'
+        'title': title,
+        'links_menu': links_menu,
+        'same_products': same_products
     }
-    return render(request, 'mainapp/products.html', context=context)
+
+    return render(request, 'mainapp/products.html', context)
+
 
